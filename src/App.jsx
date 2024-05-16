73cc6fb87822f43139b3848efa0a9ff64d49917f
joec11 <jc625@njit.edu>
2024-05-16 18:26:40 -0400

    Modified src/App.jsx to contain new logic and calculations to for each section of the Final Grade.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [inputFields, setInputFields] = useState([{ percentage: '', grades: [{ grade: '' }], result: '' }]);
+  const [inputFields, setInputFields] = useState([{ sectionPercentage: '', sectionGrades: [{ grade: '' }] }]);
+  const [overallPercentage, setOverallPercentage] = useState('');
+  const [overallResult, setOverallResult] = useState('');
+  const [overallAverage, setOverallAverage] = useState('');
 
-  const handleFormChange = (index, subIndex, event) => {
-    const { name, value } = event.target;
+  useEffect(() => {
+    updateOverall();
+  }, [inputFields]);
+
+  const handleFormChange = (index, subIndex, name, value) => {
     const updatedFields = [...inputFields];
+    updatedFields[index][name] = value;
 
-    if (name === 'percentage' || name === 'grade') {
-      updatedFields[index][name] = value;
-
-      if (name === 'grade') {
-        updatedFields[index].grades[subIndex][name] = value;
-      }
-
-      updatedFields[index].result = calculateResult(updatedFields[index]);
-      setInputFields(updatedFields);
+    if (name === 'grade') {
+      updatedFields[index].sectionGrades[subIndex][name] = value;
     }
-  };
 
-  const calculateResult = (input) => {
-    const sumOfGrades = input.grades.reduce((acc, curr) => acc + parseFloat(curr.grade || 0), 0);
-    const totalGrades = input.grades.length;
-    const percentage = parseFloat(input.percentage || 0) / 100;
-
-    return percentage !== 0 && totalGrades !== 0 ? (sumOfGrades / totalGrades * percentage).toFixed(2) : '';
-  };
-
-  const addInputField = () => setInputFields([...inputFields, { percentage: '', grades: [{ grade: '' }], result: '' }]);
-  
-  const addOrRemoveGradeField = (index, subIndexToRemove = -1) => {
-    const updatedFields = [...inputFields];
-
-    if (subIndexToRemove === -1) updatedFields[index].grades.push({ grade: '' });
-    else if (updatedFields[index].grades.length > 1) updatedFields[index].grades.splice(subIndexToRemove, 1);
-
-    updatedFields[index].result = calculateResult(updatedFields[index]);
+    updateSectionCalculations(updatedFields[index]);
     setInputFields(updatedFields);
   };
 
-  const removeInputField = (indexToRemove) => inputFields.length > 1 && setInputFields(inputFields.filter((_, index) => index !== indexToRemove));
+  const updateSectionCalculations = (input) => {
+    const sumOfGrades = input.sectionGrades.reduce((acc, curr) => acc + parseFloat(curr.grade || 0), 0);
+    const totalGrades = input.sectionGrades.filter(grade => grade.grade !== '').length;
+    const percentage = parseFloat(input.sectionPercentage || 0) / 100;
 
-  const { resultSum, percentageSum } = inputFields.reduce((acc, { result, percentage }) => ({ resultSum: acc.resultSum + parseFloat(result || 0), percentageSum: acc.percentageSum + parseFloat(percentage || 0) }), { resultSum: 0, percentageSum: 0 });
+    input.sectionResult = percentage !== 0 && totalGrades !== 0 ? (sumOfGrades / totalGrades * percentage).toFixed(3) : '';
+    input.sectionAverage = percentage !== 0 && input.sectionResult !== '' ? ((input.sectionResult / input.sectionPercentage) * 100).toFixed(3) : '';
 
-  const finalNumericGrade = (percentageSum !== 0 ? (resultSum / percentageSum * 100) : 0).toFixed(2);
+    setInputFields(input);
+  };
+
+  const updateOverall = () => {
+    const sumOfSectionResults = inputFields.reduce((acc, { sectionResult }) => acc + parseFloat(sectionResult || 0), 0);
+    let totalSectionPercentage = 0;
+
+    inputFields.forEach(({ sectionGrades, sectionPercentage }) => {
+      if (sectionGrades.some(grade => grade.grade !== '')) {
+        totalSectionPercentage += parseFloat(sectionPercentage || 0);
+      }
+    });
+
+    const overallResult = sumOfSectionResults !== '' ? sumOfSectionResults.toFixed(3) : '';
+    const overallPercentage = totalSectionPercentage !== 0 ? totalSectionPercentage : 0;
+    const overallAverage = overallPercentage !== 0 && overallResult !== '' ? ((sumOfSectionResults / totalSectionPercentage) * 100).toFixed(3) : '';
+
+    setOverallResult(overallResult);
+    setOverallPercentage(overallPercentage);
+    setOverallAverage(overallAverage);
+  };
+
+  const addInputField = () => setInputFields([...inputFields, { sectionPercentage: '', sectionGrades: [{ grade: '' }] }]);
+
+  const addOrRemoveGradeField = (index, subIndexToRemove = -1) => {
+    const updatedFields = [...inputFields];
+
+    if (subIndexToRemove === -1) {
+      updatedFields[index].sectionGrades.push({ grade: '' });
+    } else {
+      if (updatedFields[index].sectionGrades.length > 1) {
+        updatedFields[index].sectionGrades.splice(subIndexToRemove, 1);
+        updateSectionCalculations(updatedFields[index]);
+      }
+    }
+
+    setInputFields(updatedFields);
+  };
+
+  const removeInputField = (indexToRemove) => {
+    if (inputFields.length > 1) {
+      const updatedFields = inputFields.filter((_, index) => index !== indexToRemove);
+      setInputFields(updatedFields);
+    }
+  };
 
   return (
     <div className="App">
@@ -53,35 +84,56 @@ function App() {
         {inputFields.map((input, index) => (
           <div key={index}>
             <input
-              name="percentage"
+              name="sectionPercentage"
               placeholder="Percentage (%)"
-              value={input.percentage}
-              onChange={(event) => handleFormChange(index, 0, event)}
+              value={input.sectionPercentage}
+              onChange={(event) => handleFormChange(index, 0, event.target.name, event.target.value)}
             />
-            {input.grades.map(({ grade }, subIndex) => (
+            {input.sectionGrades.map(({ grade }, subIndex) => (
               <div key={subIndex}>
                 <input
                   name="grade"
                   placeholder="Grade"
                   value={grade}
-                  onChange={(event) => handleFormChange(index, subIndex, event)}
+                  onChange={(event) => handleFormChange(index, subIndex, event.target.name, event.target.value)}
+                  disabled={input.sectionPercentage === ''}
                 />
-                {input.grades.length > 1 && (
-                  <button type="button" name="Remove Grade" onClick={() => addOrRemoveGradeField(index, subIndex)}>Remove Grade</button>
+                {input.sectionGrades.length > 1 && (
+                  <button
+                    type="button"
+                    name="Remove Grade"
+                    onClick={() => addOrRemoveGradeField(index, subIndex)}
+                  >
+                    Remove Grade
+                  </button>
                 )}
               </div>
             ))}
-            <button type="button" name="Add Another Grade" onClick={() => addOrRemoveGradeField(index)}>Add Another Grade</button>
+            <button
+              type="button"
+              name="Add Another Grade"
+              onClick={() => addOrRemoveGradeField(index)}
+              disabled={input.sectionPercentage === ''}
+            >
+              Add Another Grade
+            </button>
             {inputFields.length > 1 && (
               <button type="button" name="Remove Field" onClick={() => removeInputField(index)}>Remove Field</button>
+            )}
+            {input.sectionAverage && (
+              <div name="Section Grade">
+                <label>{input.sectionResult} / {input.sectionPercentage} = <b>{input.sectionAverage}</b></label>
+              </div>
             )}
           </div>
         ))}
       </form>
       <button type="button" name="Add Field" onClick={addInputField}>Add Field</button>
-      <div name="Final Grade">
-        <label>{resultSum} / {percentageSum} = <b>{finalNumericGrade}</b></label>
-      </div>
+      {overallAverage && (
+        <div name="Final Grade">
+          <label>{overallResult} / {overallPercentage} = <b>{overallAverage}</b></label>
+        </div>
+      )}
     </div>
   );
 }
